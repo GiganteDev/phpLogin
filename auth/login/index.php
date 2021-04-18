e38f1514fdf703639b9555339b61e46763d31625
Gigante <q1dcc21@gmail.com>
2021-04-18 18:28:04 -0300

    feat(auth/login): create the page of login

---
+++ auth/login/index.php
@@ -0,0 +1,5 @@
+<form action="../../db/login/" method="POST">
+        <input name="user" type="text" placeholder="Nome de usuário">
+        <input name="pass" type="password" placeholder="Senha">
+    <button>Log in</button>
+</form>
