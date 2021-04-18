f1c2c1a685f57b3edefde82e43d6cf14a45a8ebf
Gigante <q1dcc21@gmail.com>
2021-04-18 18:28:15 -0300

    feat(auth/login): create the page of register

---
+++ auth/register/index.php
@@ -0,0 +1,6 @@
+<form action="../../db/register/" method="POST">
+    <input name="email" type="email" class="form-control form-control-xl" placeholder="Email">
+    <input name="user" type="text" class="form-control form-control-xl" placeholder="Nome de usuário">
+    <input name="pass" id="pass" type="password" class="form-control form-control-xl" placeholder="Senha">
+    <button>Registre-se</button>
+</form>
